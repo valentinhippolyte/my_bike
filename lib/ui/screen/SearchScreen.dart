a0aa3a57a0e534cd947b4a8e8c90b72c7f4f4304
denisjunior <denisdalmeida04@gmail.com>
2023-06-17 20:25:10 +0200

    update the searchscreen file to pull develop

--- lib/ui/screen/SearchScreen.dart
+++ lib/ui/screen/SearchScreen.dart
@@ -1,57 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import '../../data/dataSource/remote/VlilleApi.dart';
+import '../../data/model/VlilleResponse.dart';
 import '../assets/Colors.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
-  _SearchBarState createState() => _SearchBarState();
+  SearchScreenState  createState() => SearchScreenState();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Search "),
-        backgroundColor: MBColors.gris,
-      ),
-      backgroundColor: MBColors.grisPerle,
-
-    );
-  }
-}
-
-class _SearchBarState extends State<SearchScreen> {
-   TextEditingController _textEditingController = TextEditingController();
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
-  //List<Records> listToDisplay = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      controller: _textEditingController,
-      decoration: InputDecoration(
-        hintText: 'Rechercher...',
-        prefixIcon: Icon(Icons.search),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        ),
-      ),
-      onChanged: (value) {
-
-      },
-    );
-  }
 }
 
 
-// _list.where((fields)=>
-// fields.nom.contains(searchValue)||
-// fields.commune.contains(searchValue)||
-// fields.adresse.contains(searchValue)
-// ).toList(),
+
+class SearchScreenState  extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
